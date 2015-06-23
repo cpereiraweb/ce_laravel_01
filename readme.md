b64a8959c408fe717d5a57d8ad4d1dabfa062412
Claudio Pereira <cpereiraweb@gmail.com>
2015-06-22 21:10:22 -0300

    Commit para enviar ao CE

---
+++ readme.md
@@ -0,0 +1,7 @@
+## Code.Education - Curso de Laravel 5
+
+Clone de um projeto básico Laravel 5.0 para avaliação
+
+### License
+
+The Laravel framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
---
+++ resources/views/exemplo.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Exemplo</title>
+</head>
+<body>
+<h1>Exemplo</h1>
+<p>Olá {{ $nome }} {{ $sobrenome }}</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam commodi consectetur delectus, dolor doloribus explicabo fugit iusto magni molestiae molestias nihil numquam porro quas, quasi quibusdam quo reprehenderit sit vero.</p>
+{{ Debugbar::info($nome) }}
+</body>
+</html>
